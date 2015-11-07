111491aa873de05408003a6d959ffa130851d493
Marin Marinov <marin.telerik@gmail.com>
2015-11-07 15:39:26 +0200

    added Workshop 1

---
+++ Workshop01JediMeditation/Workshop01JediMeditation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workshop01JediMeditation
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int numberOfJedis = int.Parse(Console.ReadLine());
+
+            string[] input = Console.ReadLine().Split(' ');
+
+            Queue<string> jediMasters = new Queue<string>();
+            Queue<string> jediKnights = new Queue<string>();
+            Queue<string> jediPadawans = new Queue<string>();
+
+            for (var i = 0; i < numberOfJedis; i++)
+            {
+                if (input[i][0] == 'm')
+                {
+                    jediMasters.Enqueue(input[i]);
+                }
+                if (input[i][0] == 'k')
+                {
+                    jediKnights.Enqueue(input[i]);
+                }
+                if (input[i][0] == 'p')
+                {
+                    jediPadawans.Enqueue(input[i]);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            while (jediMasters.Count > 0)
+            {
+                var currentJedi = jediMasters.Dequeue();
+                sb.Append(currentJedi + " ");
+            }
+
+            while (jediKnights.Count > 0)
+            {
+                var currentJedi = jediKnights.Dequeue();
+                sb.Append(currentJedi + " ");
+            }
+
+            while (jediPadawans.Count > 0)
+            {
+                var currentJedi = jediPadawans.Dequeue();
+                sb.Append(currentJedi + " ");
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+    }
+}
